e724b5dc6eea56ccd141bf3e85d6dfcdf9fe0db5
Joohyun Lee <leejoohyun_my@naver.com>
2022-03-19 23:53:05 +0900

    배열의 요소에 값을 읽어서 역순으로 정렬합니다

---
+++ 02-DataStructure-basic/src/chap01/D_ReverseArray.java
@@ -0,0 +1,43 @@
+//배열의 요소에 값을 읽어 들여 역순으로 정렬합니다
+package chap01;
+
+import java.util.Scanner;
+
+public class D_ReverseArray {
+	static void swap(int a[], int idx1, int idx2) {
+		int t = a[idx1]; a[idx1]=a[idx2]; a[idx2]=t;
+	}
+	static void reverse(int a[]) {
+		for(int i=0; i<a.length/2; i++) {
+			swap(a, i, a.length-i-1);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("배열의 요솟수: ");
+		int num=scanner.nextInt();
+		int x[]=new int[num]; //요솟수가 num인 배열
+		
+		for(int i=0; i<num; i++) {
+			System.out.println("x["+i+"] = ");
+			x[i]= scanner.nextInt();
+		}
+		
+		reverse(x); //배열 a의 요소를 역순으로 정렬 
+		
+		System.out.println("요소를 역순으로 정렬했습니다.");
+		for(int i=0; i<num; i++) {
+			System.out.println("x["+i+"] = "+x[i]);
+		}
+		scanner.close();
+	}
+}
+
+
+
+
+
+
+
+
+
