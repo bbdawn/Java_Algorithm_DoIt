c749938fa8ed7534b83193d8b9403855bd31a59b
Joohyun Lee <leejoohyun_my@naver.com>
2022-03-19 20:54:36 +0900

    곱셈표 출력하기

--- 01-JavaAlgorithm-basic/src/chap02/D1.java
+++ 01-JavaAlgorithm-basic/src/chap02/D1.java
@@ -2,5 +2,12 @@
 package chap02;
 
 public class D1 {
-
+	public static void main(String[] args) {
+		for(int a=1; a<=9;a++) {
+			for(int b=1; b<=9;b++) {
+				System.out.print(a +"*"+ b + "=" + a*b+ " ");
+			}
+			System.out.println(" ");
+		}
+	}
 }
