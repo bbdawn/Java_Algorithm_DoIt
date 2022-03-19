9bca4d5d64f2fa5a949ed65f1984baa8021583d3
Joohyun Lee <leejoohyun_my@naver.com>
2022-03-19 20:54:57 +0900

    왼쪽 아래가 직각인 이등변 삼각형 출력하기

--- 01-JavaAlgorithm-basic/src/chap02/E1.java
+++ 01-JavaAlgorithm-basic/src/chap02/E1.java
@@ -2,5 +2,11 @@
 package chap02;
 
 public class E1 {
-
+	public static void main(String[] args) {
+		for(int i=1;i<5;i++) { //i는 1->2->3->4
+			for(int j=1; j<=i; j++) //j는 1->2->...->i번 반복하고 i로 돌아가기
+				System.out.print("*");
+				System.out.println(" ");
+		}
+	}
 }
